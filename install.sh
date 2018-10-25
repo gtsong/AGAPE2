ccfc1c7be0669d3519026653b8fcb23812cb0a91
gtsong <giltae.song@gmail.com>
2018-10-25 17:59:24 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 sudo dpkg --add-architecture i386
 
 sudo apt-get update -y
-sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
 sudo apt-get install -y libc6:i386 libncurses5:i386 libstdc++6:i386
