e6c5470c430e83858657e69723edfac7bb857b8a
HoYong LEE <dwhylee@gmail.com>
2019-09-26 11:20:31 +0900

    Update AutomakeRepeat.sh

--- AutomakeRepeat.sh
+++ AutomakeRepeat.sh
@@ -1,10 +1,29 @@
-perl ./configure <<END_OF_RESPONSES
-
-
-
-/usr/local/bin/trf409.linux64
-2
-/usr/bin
-Y
-5
-END_OF_RESPONSES
+#!/bin/sh
+expect <<EOF
+set timeout 3
+spawn perl ./configure
+expect "*CONTINUE>"
+send "\n"
+expect eof
+expect "*/usr/bin/perl ]:"
+send "\n"
+expect eof
+expect "*/home/AGAPE2/RepeatMasker ]:"
+send "\n"
+expect eof
+expect "*]:"
+send "/usr/local/bin/trf409.linux64\n"
+expect eof
+expect "Enter Selection"
+send "2\n"
+expect eof
+expect "Enter path [  ]:"
+send "/usr/bin\n"
+expect eof
+expect "*]:"
+send "Y\n"
+expect eof
+expect "Enter Selection"
+send "5\n"
+expect eof
+EOF
