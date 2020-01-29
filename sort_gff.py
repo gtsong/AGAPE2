21418153c52c253e15ebfeb330160bdbeedd50c3
HoYong Lee <dwhylee@gmail.com>
2020-01-29 16:22:58 +0900

    Update sort_gff.py

--- sort_gff.py
+++ sort_gff.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 import sys
 import os
+from functools import reduce
+
+def roman2int2(roman):
+    def getdv(r):
+        pos = 'IVXLCDMF'.find(r)
+        return 0 if pos < 0 else 10**(pos/2)*(1+(4*(pos%2)))
+    v = map(getdv, roman)
+    return reduce(lambda s,x: s+x[0] if x[0]>=x[1] else s-x[0], zip(v, v[1:]+[0]),0)
 
 def read_gff(gff_file):
     fp = open(gff_file,'r')
-    gffs = sorted([line.split(' ') for line in fp], key=lambda x: (x[0],int(x[3])))
+    gffs = sorted([line.split(' ') for line in fp], key=lambda x: (roman2int2(x[0].split('r')[1]),int(x[3]))))
     return gffs
 
 def write_gff(all_data):
